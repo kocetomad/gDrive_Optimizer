721bc9739f301015e96be678b38f1919e80a9c0e
Stefan Pruneanu <stefanpruneanu@gmail.com>
2020-10-27 10:55:53 +0200

    implemented Google login logic to main App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 import TreeView from './TreeView.js'
-import GDriveLogIn from "./GDriveLogIn";
+import {Nav, Navbar} from "react-bootstrap";
+import {GoogleLogin, GoogleLogout} from "react-google-login";
+
+const API_KEY = "[AIzaSyBcNCZZIEzsBOI8cyoj-Cb3e8lJe4Qg1Lk]"
+const CLIENT_ID = "796101496132-pif752amrr7vjvq8jqsvo9rf18mg4t24.apps.googleusercontent.com"
 
 const API_OUTPUT = {
   "kind": "drive#fileList",
@@ -73,22 +76,92 @@ const API_OUTPUT = {
 }
 
 function App() {
+  const [userName, setUserName] = useState(null);
+  const [loggedInState, setLoggedInState] = useState(false);
+
+  let onLoginSuccess = (response) => {
+    console.log(response)
+    console.log({result: "Login Successful.", output: "Token - " + response.tokenObj.access_token})
+
+    let userProfile = response.profileObj
+    setUserName(userProfile.givenName + " " + userProfile.familyName)
+
+    setLoggedInState(true)
+    //getFiles(response.tokenObj.access_token)
+  }
+
+  // what happens when you try to login and process fails
+  let onLoginFail = (response) => {
+    console.log({result: "Login failed.", output: response})
+  }
+
+  // what happens when you log out from Google
+  let onLogoutSuccess = () => {
+    console.log({result: "Logout successful."})
+    setLoggedInState(false)
+  }
+
+  let getFiles = (accessToken) => {
+    console.log(accessToken)
+    fetch( // main request
+      'https://www.googleapis.com/drive/v3/files?key=' + API_KEY,
+      {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + accessToken,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+      .then(async response => {
+        const data = await response.json();
+
+        // check for error response
+        if (!response.ok) {
+          // get error message from body or default to response statusText
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+        }
+
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error({response: 'Error performing fetch!', error: error});
+      })
+  }
+
   return (
     <div className="App">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand>gDrive Optimizer</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            {/*<Nav.Link href="#home">Home</Nav.Link>*/}
+            {/*<Nav.Link href="#link">Link</Nav.Link>*/}
+          </Nav>
+          <div style={{paddingRight: "10px"}}>
+            {loggedInState && <div>Logged in as <b>{userName}</b></div>}
+          </div>
+
+          {!loggedInState && <GoogleLogin
+            clientId={CLIENT_ID}
+            buttonText="Login"
+            scope="https://www.googleapis.com/auth/drive"
+            onSuccess={onLoginSuccess}
+            onFailure={onLoginFail}
+            cookiePolicy={'single_host_origin'}
+          />}
+
+          {loggedInState && <GoogleLogout
+            clientId={CLIENT_ID}
+            buttonText="Logout"
+            onLogoutSuccess={onLogoutSuccess}
+          />}
+        </Navbar.Collapse>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <GDriveLogIn/>
         <TreeView files={API_OUTPUT.files}/>
       </header>
     </div>
