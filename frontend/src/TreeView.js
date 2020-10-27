cfdcdb126e67e3c352f3a68f5b6e841a4957bd10
Stefan Pruneanu <stefanpruneanu@gmail.com>
2020-10-27 10:56:17 +0200

    migrated TreeView to react hooks and functions

--- frontend/src/TreeView.js
+++ frontend/src/TreeView.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import {Card, ListGroup, ListGroupItem} from "react-bootstrap";
@@ -9,77 +9,60 @@ Get root folder - do a GET list request with 'root' as FOLDER_ID
 Get list of files from root Google Drive folder - GET https://www.googleapis.com/drive/v3/files?q=parents%3D'root'&key=[YOUR_API_KEY]
 * */
 
-class TreeView extends React.Component {
+function TreeView(props) {
   //TODO: Maybe add that scan function here? Refer to the other TODO.
   // IDK how to properly define it so it matches ReactJS workflow and standards
-  render() {
-    return (
-      <Card>
-        <TreeElement title={"Root Folder (which also has an ID)"}/>
-        <TreeElements elements={this.props.files}/>
-      </Card>
-    );
-  }
+
+  return (
+    <Card>
+      <Card.Title><h2>Something</h2></Card.Title>
+      <TreeElement title={"Root Folder (which also has an ID)"}/>
+      <TreeElements elements={props.files}/>
+    </Card>
+  )
 }
 
-class TreeElements extends React.Component {
+function TreeElements(props) {
   //TODO: Make a function that scans each element from root and checks if there is any more stuff inside them.
   // Can be same method used to scan for a specific element.
 
-  render() {
-    return (
+  return (
+    <ListGroup variant="flush" style={{paddingLeft: "20px"}}>
+      <TreeElement title={props.elements[0].id}/>
+      <TreeElement title={props.elements[1].id}/>
       <ListGroup variant="flush" style={{paddingLeft: "20px"}}>
-        <TreeElement title={this.props.elements[0].id}/>
-        <TreeElement title={this.props.elements[1].id}/>
-        <ListGroup variant="flush" style={{paddingLeft: "20px"}}>
-          <TreeElement title={this.props.elements[2].id}/>
-          <TreeElement title={this.props.elements[3].id}/>
-          <TreeElement title={this.props.elements[4].id}/>
-        </ListGroup>
-        <TreeElement title={this.props.elements[5].id}/>
+        <TreeElement title={props.elements[2].id}/>
+        <TreeElement title={props.elements[3].id}/>
+        <TreeElement title={props.elements[4].id}/>
       </ListGroup>
-    )
-  }
+      <TreeElement title={props.elements[5].id}/>
+    </ListGroup>
+  )
 }
 
-class TreeElement extends React.Component {
-  constructor(props) {
-    super(props);
+function TreeElement(props) {
+  const [title, setTitle] = useState(props.title)
+  const [icon, setIcon] = useState(null)
 
-    // we use isParent to note if the element has more elements inside (a folder with files) so we show dropdown arrow
-    // or the element is a file or an empty folder, in which case we don't show a dropdown arrow
-    this.state = {title: props.title, icon: props.icon}
-  }
-
-  componentDidMount() {
-    // this function is called whenever the element is rendered to the DOM
-  }
-
-  componentWillUnmount() {
-    // this function is called whenever the element is removed from the DO
-  }
-
-  render() {
-    return (
-      <ListGroupItem action>
-        <table>
-          <tbody>
-          <tr>
-            <td style={{paddingRight: "10px"}}>
-              <ArrowDropDown/>
-            </td>
-            <td style={{paddingRight: "10px"}}>
-              <Folder/>
-            </td>
-            <td>
-              <span>{this.state.title}</span>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </ListGroupItem>
-    )
-  }
+  return (
+    <ListGroupItem action>
+      <table>
+        <tbody>
+        <tr>
+          <td style={{paddingRight: "10px"}}>
+            <ArrowDropDown/>
+          </td>
+          <td style={{paddingRight: "10px"}}>
+            <Folder/>
+          </td>
+          <td>
+            {title}
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </ListGroupItem>
+  )
 }
 
 export default TreeView;
