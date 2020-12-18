5a0f81e4d467718e81983f3d360a081a4f4b5b17
Stefan Pruneanu <stefanpruneanu@gmail.com>
2020-12-18 14:07:36 +0200

    added Wizard. Cleanup CSS.

--- frontend/src/CompressionMethodComponent.js
+++ frontend/src/CompressionMethodComponent.js
@@ -1,9 +1,19 @@
-import {Alert, Button, Card, Col, Form, Row} from "react-bootstrap";
+import {Carousel, Alert, Button, Card, Col, Form, Row} from "react-bootstrap";
 import React, {useState} from "react";
+
+import image1 from './images/1.png';
+import image2 from './images/2.png';
+import image3 from './images/3.png';
 
 function CompressionMethodComponent(props) {
   const title = "Compression Method"
   const [requestMessage, setRequestMessage] = useState("idle");
+
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
 
   /**
    * @description This function sends the list of files to compress to the backend.
@@ -67,13 +77,38 @@ function CompressionMethodComponent(props) {
         </ul>
         {requestMessage === "sent" &&
         <Alert key={'primary'} variant={'primary'}>
-          Compression started. You will be notified via email when it's done.
+          Compression started. You will be notified via email when it's done. In the meantime, check this guide:
         </Alert>}
+        {requestMessage === "sent" &&
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+          <Carousel.Item>
+            <img
+              className="d-block w-50"
+              style={{marginLeft: 'auto', marginRight: 'auto'}}
+              src={image1}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-50"
+              style={{marginLeft: 'auto', marginRight: 'auto'}}
+              src={image2}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-50"
+              style={{marginLeft: 'auto', marginRight: 'auto'}}
+              src={image3}
+              alt="First slide"
+            />
+          </Carousel.Item>
+        </Carousel>}
       </Card.Body>
       <Card.Footer>
-        <Button onClick={() => {
-          sendFiles(props.queue)
-        }}>Compress</Button>
+        <Button onClick={() => {sendFiles(props.queue)}}>Compress</Button>
       </Card.Footer>
     </Card>
   )
